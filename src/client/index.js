f0dbf29826243fa90623d4f0128993358abe2a0b
Joshua Webster <darkwolf62@gmail.com>
2020-07-26 01:58:54 -0500

    fixed css

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,10 @@
 import { urlValid } from './js/urlChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 
 console.log(checkForName);
 
